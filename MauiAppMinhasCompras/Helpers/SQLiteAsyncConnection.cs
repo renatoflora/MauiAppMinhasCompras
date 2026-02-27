cf490684bafb6211146029f0ae23296469fed87d
renatoflora <renato.flora88@gmail.com>
2026-02-27 10:46:48 -0300

    Implementando CRUD

---
+++ MauiAppMinhasCompras/Helpers/SQLiteAsyncConnection.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MauiAppMinhasCompras.Helpers
+{
+    internal class SQLiteAsyncConnection
+    {
+        private string path;
+
+        public SQLiteAsyncConnection(string path)
+        {
+            this.path = path;
+        }
+
+        internal object CreateTableAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
